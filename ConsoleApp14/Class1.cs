b6258f721028788a53f4dd4320cbc3669eeefd39
nicko <nicko@DESKTOP-CUQJJR3>
2022-03-28 17:42:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Phone
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public Phone() { }
+        public Phone(string name, string model)
+        {
+            Name = name;
+            Model = model;
+        }
+        public virtual void Print() =>Console.WriteLine($"Phone: Name = {Name},Model = {Model}");
+    }
+    internal class Mobile : Phone
+    {
+        string Type, Date;
+        public Mobile(string Name,string Model,string type, string date) : base(Name, Model)
+        {
+            Type = type;
+            Date = date;
+        }
+        public override void Print() =>Console.WriteLine($"Мобильный телефон: Название - {Name}, Модель - {Model}, Тип - {Type}, Дата - {Date}");
+    }
+    internal class Landline : Phone
+    {
+        string Number;
+        public Landline(string Name, string Model, string number) : base(Name, Model)
+        {
+            Number = number;
+        }
+        public override void Print() => Console.WriteLine($"Стационарный телефон: Название - {Name}, Модель - {Model}, Номер - {Number}");
+    }
+
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile s = new Mobile("Samsung", "Galaxy  S20", "mobile phone", "12.03.2022");
+            s.Print();
+            Landline c = new Landline("Panasonic", "KX-TS2350","84953622736");
+            c.Print();
+            Console.ReadKey();
+        }
+    }
+}
